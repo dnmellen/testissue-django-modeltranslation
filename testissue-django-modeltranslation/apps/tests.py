5e004267e998deeb31714b756810953f404295d3
Diego Navarro Mellén <dnavarro@qdqmedia.com>
2013-09-26 13:33:48 +0200

    More tests

--- testissue-django-modeltranslation/apps/tests.py
+++ testissue-django-modeltranslation/apps/tests.py
@@ -3,26 +3,42 @@ from django.utils.translation import trans_real
 from .models import Category
 
 
-class BasicTest(TestCase):
+class CategoryCreatedMixin(object):
+
+    def setUp(self):
+        # Here is the problematic code but works here!!
+        Category.objects.create(name="Category one")
+
+
+class BasicTest(CategoryCreatedMixin, TestCase):
 
     def test_create_category(self):
 
-        # Here is the problematic code!!
-        Category.objects.create(name="Category one")
         # Retrieve category
         cat = Category.objects.get(name="Category one")
         self.assertEquals(cat.name, "Category one")
 
-    def test_create_category_eng(self):
+    def test_multiple_langs(self):
 
-        # Here is the problematic code!!
+        category = Category.objects.get(name="Category one")
+
         trans_real.activate('en')
-        category = Category()
-        category.name = "Category one"
+        category.name = "Category one english"
+        category.save()
+        trans_real.deactivate()
+
+        trans_real.activate('es')
+        category.name = "Categoria uno"
         category.save()
         trans_real.deactivate()
 
         # Retrieve category
-        cat = Category.objects.get(name="Category one")
-        self.assertEquals(cat.name, "Category one")
-        self.assertEquals(cat.name_en, "Category one")
+        trans_real.activate('en')
+        cat = Category.objects.all()[0]
+        self.assertEquals(cat.name, "Category one english")
+        trans_real.deactivate()
+
+        trans_real.activate('es')
+        cat = Category.objects.all()[0]
+        self.assertEquals(cat.name, "Categoria uno")
+        trans_real.deactivate()
